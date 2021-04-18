f4048e7b2356fdcf1e090e9c8fea64f8df31e0d7
bogdanpetrovsky <bpetrovsky977@gmail.com>
2021-04-18 12:43:17 +0300

    fixed 'component generating with css styles' issue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
+import { Main2Component } from './main2/main2.component';
+import { Main1Component } from './main1/main1.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    Main2Component,
+    Main1Component
   ],
   imports: [
     BrowserModule
